7a093f1ade5d6b28f5cfad8848fd6cd33ca89c5b
B-a-Zz <clement.f.bazz@gmail.com>
2020-10-10 18:50:51 +0100

    index and home

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,10 +6,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HOMEComponent implements OnInit {
-
-  constructor() { }
+  targetClient:any;
+  constructor() {
+    this.targetClient =
+    [{client: 'Individuals',info:'Want to send regular parcels as individual customer ?'},
+    {client: 'Business',info:'Want to offer to your customers a high quality delivery experience?'},
+    {client:'Devs', info:'Want to be part of our happy and independent couriers community ?'}
+  ]
+  }
 
   ngOnInit(): void {
   }
+
 
 }
